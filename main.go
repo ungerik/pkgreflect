c3d27eca9db93a4c11bf81ca6c9587970ccec2f1
Erik Unger <erik@erikunger.com>
2014-07-10 19:12:36 +0200

    added docu from readme to main

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
+/*
+Go reflection does not support enumerating types, variables and functions of packages.
+
+pkgreflect generates a file named pkgreflect.go in every parsed package directory.
+This file contains the follwing maps of exported names to reflection types/values:
+
+	var Types = map[string]reflect.Type{ ... }
+	var Functions = map[string]reflect.Value{ ... }
+	var Variables = map[string]reflect.Value{ ... }
+
+Command line usage:
+
+	pkgreflect --help
+	pkgreflect [-notypes][-nofuncs][-novars][-unexported][-norecurs][-gofile=filename.go] [DIR_NAME]
+
+If -norecurs is not set, then pkgreflect traverses recursively into sub-directories.
+If no DIR_NAME is given, then the current directory is used as root.
+*/
 package main
 
 import (
